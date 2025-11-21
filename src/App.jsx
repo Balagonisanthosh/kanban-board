76b15e520f6ec7685618db3b68c7b2729e68af90
santhosh <balagonisanthosh445@gmail.com>
2025-11-21 22:18:33 +0530

    Configure Vite base and package.json for Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { BrowserRouter, Routes, Route, Form } from "react-router-dom";
 import './index.css';
-import KanbanBoard from "./Pages/KanbanBoard";
 import { FormProvider } from "./Components/FormContext";
 import Mainkanbanboard from "./Components/KanbanBoard/Main-kanbanboard";
 import MainForm from "./Components/KanbanBoard/MainForm";
@@ -12,15 +11,6 @@ function App() {
   
   return (
     <div className=" min-h-screen bg-slate-200" >
-    {/* <BrowserRouter>
-    <FormProvider>
-    <Routes>
-    <Route path="/KanbanBoard" element={<KanbanBoard />}/>
-
-    </Routes>
-    </FormProvider>
-    
-    </BrowserRouter> */}
 
      <Mainkanbanboard/> 
        
